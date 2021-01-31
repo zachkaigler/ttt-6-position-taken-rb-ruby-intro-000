83fa9d561c09020cbb1d98b62f0f70f4c86a4abf
Zach Kaigler <zachkaiglerfilm@gmail.com>
2021-01-31 18:04:14 +0000

    Automatically backed up by Learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -1,7 +1,7 @@
 board = [" "," "," "," "," "," "," "," "," "]
 
 def position_taken?(board, index)
-  if board[index] == " " || board[index] == ""
+  if board[index] == " " || board[index] == "" || board[index] == nil
     false
   end
 end
